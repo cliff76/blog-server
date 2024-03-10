d307ee719eb54155430cb02bd9b895735c9d1c65
cliff76 <270462+cliff76@users.noreply.github.com>
2024-03-10 16:51:40 -0700

    Makes API changes for Vercel deployment.
    Adds Vercel config.
    Creates an API folder.
    Changes default port to 3000.

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(bodyParser.json());
 
